977408f34dc26fc14129e7fbf26149ff4293ed92
Emilse Ormeno <eugecodes@pm.me>
2022-08-10 00:20:28 -0300

    Update

--- app/src/main/java/com/example/catapp/data/network/ApiService.kt
+++ app/src/main/java/com/example/catapp/data/network/ApiService.kt
@@ -10,7 +10,7 @@ class ApiService {
     private val retrofit = RetrofitHelper.getRetrofit()
     suspend fun getBreeds(): CatsResponse {
         return withContext(Dispatchers.IO) {
-            var response = retrofit.create(ApiClient::class.java).getCatsByBreeds("breeds?limit=50&api_key=eeaafac8-5268-4542-a4f5-72127eb38a55/")
+            var response = retrofit.create(ApiClient::class.java).getCatsByBreeds("breeds?limit=5000&api_key=eeaafac8-5268-4542-a4f5-72127eb38a55/")
             println(response.body())
             response.body() !!
         }
